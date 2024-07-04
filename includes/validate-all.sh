7d25d57bf75f38d7c0f9d29ec8b0cef79f856677
kwatsen <kent@watsen.net>
2024-07-04 17:17:33 -0400

    enabling validation finds bugs!

--- includes/validate-all.sh
+++ includes/validate-all.sh
@@ -19,4 +19,17 @@ run_unix_cmd() {
   fi
 }
 
-echo "Validation currently DISABLED (FIXME: validate all examples!)"
+DATE=`date +%Y-%m-%d`
+
+# FIXME: add validation for ex-api-combo-rpc-reply.xml
+#   ^-- this is the only INSERTED file containing a validatable payload...
+#   ^-- seems like the HTML-header should be stripped off and the rest validated against example-social.yang?
+#   ^-- but where does the "xml-list" come from? !!!
+#          ^-- oh, I see it's defined in this draft but:
+#                - note that the example in the -nc draft also use the "xml-list" media-type - why?
+#                    - should the "xml-list" be defined in a neutral document?
+printf "Testing ex-api-combo-rpc-reply.xml..."
+command="yanglint -t reply ../../list-pagination/includes/example-social@$DATE.yang ex-api-combo-rpc-reply.xml"
+run_unix_cmd $LINENO "$command" 0
+printf "okay.\n"
+ 
