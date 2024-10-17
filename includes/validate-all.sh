ca07815c07fb1ea9a4968866b736242bfe5f0439
kwatsen <kent@watsen.net>
2024-10-17 14:07:47 -0400

    fixed validation.

--- includes/validate-all.sh
+++ includes/validate-all.sh
@@ -21,18 +21,20 @@ run_unix_cmd() {
 
 DATE=`date +%Y-%m-%d`
 
-printf "Testing ex-api-combo-rpc-reply.xml...DISABLED!\n" # how to validate xml-list?
-#tail -n+7 ex-api-combo-rpc-reply.xml > ex-api-combo-rpc-reply.xmlonly.xml
-#command="yanglint -e -t data ietf-datastores@2018-02-14.yang ../../list-pagination/ietf-list-pagination@$DATE.yang ../../list-pagination/includes/example-social@$DATE.yang ex-api-combo-rpc-reply.xmlonly.xml"
-#run_unix_cmd $LINENO "$command" 0
-#rm -f ex-api-combo-rpc-reply.xmlonly.xml
-#printf "okay.\n"
+printf "Testing ex-api-combo-rpc-reply.xml..."
+# convert input doc to something yanglint can understand
+tail -n+7 ex-api-combo-rpc-reply.xml | sed -e 's#<xml-list#<members xmlns="https://example.com/ns/example-social"#' -e 's#xml-list#members#' -e 's/alice/bob/' > ex-api-combo-rpc-reply.xmlonly.xml
+command="yanglint -e -t data ietf-datastores@2018-02-14.yang ../../list-pagination/ietf-list-pagination@$DATE.yang ../../list-pagination/includes/example-social@$DATE.yang ex-api-combo-rpc-reply.xmlonly.xml"
+run_unix_cmd $LINENO "$command" 0
+rm -f ex-api-combo-rpc-reply.xmlonly.xml
+printf "okay.\n"
 
 
-printf "Testing ex-api-combo-rpc-reply.json...DISABLED\n" # wrap with a "members" dict?
-#tail -n+7 ex-api-combo-rpc-reply.json > ex-api-combo-rpc-reply.jsononly.json
-#command="yanglint -e -t data ietf-datastores@2018-02-14.yang ../../list-pagination/ietf-list-pagination@$DATE.yang ../../list-pagination/includes/example-social@$DATE.yang ex-api-combo-rpc-reply.jsononly.json"
-#run_unix_cmd $LINENO "$command" 0
-#rm -f ex-api-combo-rpc-reply.jsononly.json
-#printf "okay.\n"
+printf "Testing ex-api-combo-rpc-reply.json..."
+# convert input doc to something yanglint can understand
+tail -n+7 ex-api-combo-rpc-reply.json | sed -e 's/^{/{ "example-social:members": {/' -e 's/^}/}}/' -e 's/alice/bob/' > ex-api-combo-rpc-reply.jsononly.json
+command="yanglint -e -t data ietf-datastores@2018-02-14.yang ../../list-pagination/ietf-list-pagination@$DATE.yang ../../list-pagination/includes/example-social@$DATE.yang ex-api-combo-rpc-reply.jsononly.json"
+run_unix_cmd $LINENO "$command" 0
+rm -f ex-api-combo-rpc-reply.jsononly.json
+printf "okay.\n"
 
